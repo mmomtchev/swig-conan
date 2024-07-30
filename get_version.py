89de51d57594ca2cd5e89312572a96ddbaadca14
Momtchil Momtchev <momtchil@momtchev.com>
2024-07-30 18:30:50 +0200

    add SWIG-JSE 5.0.4 (#1)
    * add SWIG-JSE 5.0.4
    
    * always run CI
    
    * use separate caches per arch

--- get_version.py
+++ get_version.py
@@ -2,4 +2,6 @@ import yaml
 import sys
 
 with open(sys.argv[1]) as stream:
-    print('LATEST={}'.format(list(yaml.safe_load(stream)['versions'].keys())[0]))
+    versions = list(yaml.safe_load(stream)['versions'].keys())
+    versions.sort()
+    print('LATEST={}'.format(versions[-1]))
