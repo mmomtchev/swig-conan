eea94c9b85a24f8c803b7c0a5a2c80c96b9476d6
Momtchil Momtchev <momtchil@momtchev.com>
2025-09-30 12:52:26 +0200

    try abusing tool_requires

--- swig/all/conanfile.py
+++ swig/all/conanfile.py
@@ -53,11 +53,11 @@ class SwigConan(ConanFile):
 
     def build_requirements(self):
         if self._use_pcre2:
-            self.requires("pcre2/10.43")
+            self.tool_requires("pcre2/10.43")
         else:
-            self.requires("pcre/8.45")
+            self.tool_requires("pcre/8.45")
         if is_apple_os(self):
-            self.requires("libgettext/0.22")
+            self.tool_requires("libgettext/0.22")
         if self._settings_build.os == "Windows":
             self.win_bash = True
             if not self.conf.get("tools.microsoft.bash:path", check_type=str):
