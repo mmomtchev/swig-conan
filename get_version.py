1de254d81c970329f76278f99e409abb031ebc57
Momtchil Momtchev <momtchil@momtchev.com>
2025-11-01 23:48:43 +0100

    determine correctly latest version

--- get_version.py
+++ get_version.py
@@ -1,7 +1,8 @@
 import yaml
 import sys
+from packaging.version import Version
 
 with open(sys.argv[1]) as stream:
     versions = list(yaml.safe_load(stream)['versions'].keys())
-    versions.sort()
+    versions.sort(key=Version)
     print('LATEST={}'.format(versions[-1]))
